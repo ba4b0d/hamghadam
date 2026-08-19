bfb97781722f452b0fbc30ffd3495d87ae6de395
barbadmb <barbad.mb@gmail.com>
2026-08-19 10:15:49 +0330

    bump: release v0.3.0 with configured Google Web Client ID

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.fitnessapp.android"
         minSdk = 28
         targetSdk = 37
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 3
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
