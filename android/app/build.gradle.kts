9985bb65ce58e1f364b3555a414b82d60916b502
barbadmb <barbad.mb@gmail.com>
2026-08-19 16:03:46 +0330

    feat(brand): redesign modern logo & launcher icons for HamPa (هم پا)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.fitnessapp.android"
         minSdk = 28
         targetSdk = 37
-        versionCode = 14
-        versionName = "0.6.2"
+        versionCode = 15
+        versionName = "0.6.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
